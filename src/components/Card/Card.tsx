d53ff113197193bf33887e5eb2a3e1e1531e4a4b
Samuel Raducan <samuel.thembp@gmail.com>
2024-03-28 11:39:36 +0000

    Create a reusable card component that renders info about each beer

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,27 @@
+import { getFirstSentence } from "../../helpers/getFirstSentence";
+import "./Card.scss";
+
+type CardProps = {
+    image: string;
+    name: string;
+    tagline: string;
+    description: string;
+};
+
+const Card = ({ image, name, tagline, description }: CardProps) => {
+    return (
+        <div className="card">
+            <img className="card__image" src={image} alt={name} />
+
+            <div className="card__details">
+                <h2 className="card__title">{name}</h2>
+                <span className="card__tagline">#{tagline} </span>
+                <p className="card__description">
+                    {getFirstSentence(description)}
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,5 +1,5 @@
 import beers from "../../data/beers";
-import { getFirstSentence } from "../../helpers/getFirstSentence";
+import Card from "../Card/Card";
 import "./CardList.scss";
 
 const CardList = () => {
@@ -7,23 +7,30 @@ const CardList = () => {
         <section className="cards">
             {beers &&
                 beers.map((beer) => (
-                    <div className="card" key={beer.id}>
-                        <img
-                            className="card__image"
-                            src={beer.image_url}
-                            alt={beer.name}
-                        />
+                    <Card
+                        key={beer.id}
+                        image={beer.image_url}
+                        name={beer.name}
+                        tagline={beer.tagline}
+                        description={beer.description}
+                    />
+                    // <div className="card" key={beer.id}>
+                    //     <img
+                    //         className="card__image"
+                    //         src={beer.image_url}
+                    //         alt={beer.name}
+                    //     />
 
-                        <div className="card__details">
-                            <h2 className="card__title">{beer.name}</h2>
-                            <span className="card__tagline">
-                                #{beer.tagline}{" "}
-                            </span>
-                            <p className="card__description">
-                                {getFirstSentence(beer.description)}
-                            </p>
-                        </div>
-                    </div>
+                    //     <div className="card__details">
+                    //         <h2 className="card__title">{beer.name}</h2>
+                    //         <span className="card__tagline">
+                    //             #{beer.tagline}{" "}
+                    //         </span>
+                    //         <p className="card__description">
+                    //             {getFirstSentence(beer.description)}
+                    //         </p>
+                    //     </div>
+                    // </div>
                 ))}
         </section>
     );
